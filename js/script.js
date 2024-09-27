ff46ab84e88d4756821a8fb73bf5567319983b55
SydNichols <sydneynichols.m@gmail.com>
2024-09-26 23:51:12 -0400

    Adding in the 'other' input box
    Added a function showing an input for an unlisted job role when the 'other' option is chosen as a job title is chosen

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+//putting focus on the first input field when the DOM is loaded
 addEventListener('DOMContentLoaded', () => {
-    const userName = document.getElementById('name');
-
+    const userName = document.querySelector('input[type="text"]');
     userName.focus();
 });
+
+//showing an input for an unlisted job role when the 'other' is chosen
+const jobSelect = document.querySelector('#title');
+const otherJobInput = document.querySelector('#other-job-role');
+
+otherJobInput.style.display = 'none';
+console.log(jobOptions.value);
+
+jobSelect.addEventListener('change', () => {
+    console.log(jobSelect.value)
+
+    if (jobSelect.value === 'other') {
+        otherJobInput.disabled = false;
+        otherJobInput.style.display = 'block';
+    } else {
+        otherJobInput.style.display = 'none';
+        otherJobInput.disabled = true;
+        otherJobInput.value = '';
+    }    
+})
